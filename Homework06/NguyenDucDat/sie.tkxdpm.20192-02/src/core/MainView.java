5079a6795aa4a86bdf8e64875c988ba3ec754dda
Chung <chung1905@gmail.com>
2020-06-04 16:51:50 +0700

    Style main page

--- Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/core/MainView.java
+++ Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/core/MainView.java
@@ -9,25 +9,46 @@ import java.awt.event.ActionListener;
 public class MainView implements IView {
     private final MainController controller;
 
+    private Container rootContainer;
+
     public MainView(MainController controller) {
         this.controller = controller;
     }
 
     public Container draw() {
-        Container rootContainer = new Container();
-        // https://docs.oracle.com/javase/tutorial/uiswing/layout/border.html
-        rootContainer.setLayout(new BorderLayout(20, 20));
-
-        rootContainer.add(getButton("Phuong thuc van chuyen", e -> controller.moveToTransportation()), BorderLayout.PAGE_START);
-        rootContainer.add(getButton("Mat hang kinh doanh", e -> controller.moveToMHKD()), BorderLayout.CENTER);
-        rootContainer.add(new JButton("Quit"), BorderLayout.PAGE_END);
+        Container rootContainer = getRootContainer();
+        BoxLayout layout = new BoxLayout(rootContainer, BoxLayout.PAGE_AXIS);
+        rootContainer.setLayout(layout);
+        addBtn("Phuong thuc van chuyen", e -> controller.moveToTransportation());
+        addBtn("Mat hang kinh doanh", e -> controller.moveToMHKD());
+        addBtn("QUIT", e -> System.out.println("QUIT"));
 
         return rootContainer;
     }
 
+    private void addBtn(String text, ActionListener listener) {
+        Container rootContainer = getRootContainer();
+        rootContainer.add(createVerticalGlue());
+        rootContainer.add(getButton(text, listener));
+        rootContainer.add(createVerticalGlue());
+    }
+
     private JButton getButton(String text, ActionListener listener) {
         JButton btn = new JButton(text);
+        btn.setAlignmentX(Component.CENTER_ALIGNMENT);
         btn.addActionListener(listener);
         return btn;
+    }
+
+    private Container getRootContainer() {
+        if (rootContainer == null) {
+            rootContainer = new Container();
+        }
+
+        return rootContainer;
+    }
+
+    public Component createVerticalGlue() {
+        return new Box.Filler(new Dimension(0, 0), new Dimension(0, 0), new Dimension(0, 600));
     }
 }
