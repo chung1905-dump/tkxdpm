380f736b8c28cc2876b9abde20c54cdf2f1c9fac
Chung <chung1905@gmail.com>
2020-06-10 14:59:22 +0700

    WIP: Saving

--- Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/QuanLyMHKD/controller/MHKDEditController.java
+++ Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/QuanLyMHKD/controller/MHKDEditController.java
@@ -1,11 +1,16 @@
 package QuanLyMHKD.controller;
 
+import QuanLyMHKD.entity.MatHangKinhDoanh;
 import QuanLyMHKD.view.MHKDEditGUI;
 import main.IController;
 import main.IView;
 
 public class MHKDEditController implements IController {
     public IView run() {
-        return new MHKDEditGUI();
+        return new MHKDEditGUI(this);
+    }
+
+    public void save(MatHangKinhDoanh mhkd) {
+
     }
 }
--- Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/QuanLyMHKD/view/edit/MHKDForm.java
+++ Homework06/NguyenDucDat/sie.tkxdpm.20192-02/src/QuanLyMHKD/view/edit/MHKDForm.java
@@ -1,32 +1,68 @@
 package QuanLyMHKD.view.edit;
 
+import QuanLyMHKD.controller.MHKDEditController;
+import QuanLyMHKD.entity.MatHangKinhDoanh;
 import core.layout.SpringUtilities;
 import main.IView;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.util.HashMap;
 
 public class MHKDForm implements IView {
+    private MHKDEditController controller;
+    private HashMap<String, Component> componentMap = new HashMap<String, Component>();
+
+    public MHKDForm(MHKDEditController controller) {
+        this.controller = controller;
+    }
 
     public Container draw() {
-        Container c = new Container();
-        c.setLayout(new SpringLayout());
+        Container form = new Container();
+        form.setLayout(new SpringLayout());
 
-        String[] labels = {"Name: ", "Fax: ", "Email: ", "Address: "};
+        String[] labels = {"Ten: ", "Merchandise: ", "Quantity: ", "Unit: "};
+        String[] keys = {"name", "merchandise", "qty: ", "unit"};
         int numPairs = labels.length;
 
-        for (String label : labels) {
-            JLabel l = new JLabel(label, JLabel.TRAILING);
-            c.add(l);
+        for (int i = 0; i < labels.length; i++) {
+            JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+            form.add(l);
             JTextField textField = new JTextField(10);
             l.setLabelFor(textField);
-            c.add(textField);
+            this.addComponentByName(form, textField, keys[i]);
         }
 
-        SpringUtilities.makeCompactGrid(c,
+        JButton saveBtn = new JButton("Save");
+        saveBtn.addActionListener(e -> this.controller.save());
+
+        SpringUtilities.makeCompactGrid(form,
                 numPairs, 2,
                 6, 6,
                 6, 6);
-        return c;
+        return form;
+    }
+
+    private MatHangKinhDoanh prepareEntity(Container form) {
+        MatHangKinhDoanh mhkd = new MatHangKinhDoanh();
+
+        String name = ((JTextComponent) getComponentByName("name")).getText();
+        String merchandise = ((JTextComponent) getComponentByName("merchandise")).getText();
+        float qty = Float.parseFloat(((JTextComponent) getComponentByName("name")).getText());
+        String unit = ((JTextComponent) getComponentByName("unit")).getText();
+
+        mhkd.setName(name).setMerchandise(merchandise).setQty(qty).setUnit(unit);
+
+        return mhkd;
+    }
+
+    private Component getComponentByName(String name) {
+        return componentMap.getOrDefault(name, null);
+    }
+
+    private void addComponentByName(Container parent, Component child, String name) {
+        parent.add(child);
+        componentMap.put(name, child);
     }
 }
